1b601943221cb92bfb33da22bb9d6cc85da05bf4
Anthony Li <li.anthony917@gmail.com>
2019-01-10 23:00:58 -0500

    Initial commit

---
+++ assets/script.js
@@ -0,0 +1,21 @@
+window.addEventListener("load", () => {
+  let arcs = document.querySelector(".logo-animated .logo-arcs");
+  if (arcs) {
+    anime({
+      targets: arcs,
+      opacity: [0, 1],
+      rotate: "+=4turn",
+      duration: 1000,
+      delay: 500,
+      easing: "cubicBezier(0.165, 0.840, 0.440, 1.000)"
+    }).finished.then(() => {
+      anime({
+        targets: arcs,
+        rotate: "+=180deg",
+        duration: 3000,
+        loop: true,
+        easing: "linear"
+      });
+    });
+  }
+});
